1541f7432095d33a876f2af279e7efa41c51f5a8
Mauricio Almeida <mauriciomdea@gmail.com>
2015-08-31 13:04:24 -0300

    Basic Users API tests and User model

---
+++ beautboss/spec/requests/users_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Users API", type: :request do
+
+  describe "GET /api/v1/users/:id" do
+    it "returns a requested user" do
+      user = FactoryGirl.create :user, name: "John Doe"
+      # get "/api/v1/users/#{user.id}", {}, { "Accept" => "application/json" }
+      get api_users_path(user.id), {}, { "Accept" => "application/json" }
+      expect(response.status).to eq 200
+      body = JSON.parse(response.body)
+      expect(body["name"]).to eq "John Doe"
+    end
+  end
+
+  # describe "GET /users" do
+  #   it "works! (now write some real specs)" do
+  #     get api_users_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+end
