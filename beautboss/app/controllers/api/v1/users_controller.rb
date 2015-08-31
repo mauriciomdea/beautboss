a49139862af82b518b90c3e062ebafc0c293caac
Mauricio Almeida <mauriciomdea@gmail.com>
2015-08-31 20:31:46 -0300

    Create User and error handling

--- beautboss/app/controllers/api/v1/users_controller.rb
+++ beautboss/app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,24 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     render json: User.find(params[:id]), root: false
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "Not found"}, status: 404, root: false
   end
 
   def create
+    user = User.new(
+      name: user_params[:name],
+      email: user_params[:email],
+      password: user_params[:password]
+    )
+    if user.save
+      render json: UserSerializer.new(user).as_json,
+        location: "/api/v1/users/#{user.id}",
+        status: :created,
+        root: false
+    else
+      render json: { errors: user.errors.full_messages }, status: 422
+    end
   end
 
   def update
@@ -14,6 +29,12 @@ class Api::V1::UsersController < ApplicationController
   end
 end
 
-# def default_serializer_options
-#   {root: false}
-# end
+private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+def default_serializer_options
+  {root: false}
+end
--- beautboss/spec/requests/users_spec.rb
+++ beautboss/spec/requests/users_spec.rb
@@ -3,14 +3,53 @@ require 'rails_helper'
 RSpec.describe "Users API v1", type: :request do
 
   describe "GET /api/v1/users/:id" do
-    it "returns a requested user" do
+
+    it "returns requested user" do
       user = FactoryGirl.create :user, name: "John Doe"
       get "/api/v1/users/#{user.id}", {}, { "Accept" => "application/json" }
       # get api_v1_user(user.id), {}, { "Accept" => "application/json" }
-      expect(response.status).to eq 200
+      expect(response.status).to eq 200 # ok
       body = JSON.parse(response.body)
       expect(body["name"]).to eq "John Doe"
     end
+
+    it "returns not found error for unknown user" do
+      user = FactoryGirl.create :user
+      get "/api/v1/users/#{user.id+1}", {}, { "Accept" => "application/json" }
+      expect(response.status).to eq 404 # not found
+    end
+
+  end
+
+  describe "POST /users" do
+
+    it "creates a new user" do
+      user_params = {
+        "name" => "John Doe",
+        "email" => "john_doe@example.com",
+        "password" => "1234"
+      }.to_json
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+      post "/api/v1/users", user_params, request_headers
+      expect(response.status).to eq 201 # created
+      expect(User.first.name).to eq "John Doe"
+    end
+
+    it "returns an error for invalid user" do
+      user_params = {
+        "name" => ""
+      }.to_json
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+      post "/api/v1/users", user_params, request_headers
+      expect(response.status).to eq 422 # unprocessable entity
+    end
+
   end
 
   # describe "GET /users" do
