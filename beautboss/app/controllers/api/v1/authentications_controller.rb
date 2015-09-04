e4d5db8c7dfd5fbfdddc1f61e8a84961e6e3daa2
Mauricio Almeida <mauriciomdea@gmail.com>
2015-09-04 11:39:38 -0300

    Facebook authentication

--- beautboss/app/controllers/api/v1/authentications_controller.rb
+++ beautboss/app/controllers/api/v1/authentications_controller.rb
@@ -11,6 +11,17 @@ class Api::V1::AuthenticationsController < ApplicationController
 
   end
 
+  def create_from_facebook
+    params.each do |key,value|
+      puts "Param #{key}: #{value}"
+    end
+    puts "referer: #{request.referrer}"
+    user = FbGraph2::User.me("CAAUzZAJppt5EBAHEKR0y0lxSBNnAtAqNGq3l9eHJOKQp4RGDZC8BbQvA3XZAhVqLfHymyerAqskZCphg12Y7tOpNqYES917Wr6mZBWRYPchb9BZBfh891k3xCD8tGDOOwmdQgiF5h3iLdApzPJE2Fa115zABifwkLSY9C0VAxhDiWowSBCJIK5MhCj5azyJ8wFwLAKZA6QsZCgZDZD").fetch
+    # user = FbGraph2::User.new('user_id').authenticate("CAAUzZAJppt5EBAHEKR0y0lxSBNnAtAqNGq3l9eHJOKQp4RGDZC8BbQvA3XZAhVqLfHymyerAqskZCphg12Y7tOpNqYES917Wr6mZBWRYPchb9BZBfh891k3xCD8tGDOOwmdQgiF5h3iLdApzPJE2Fa115zABifwkLSY9C0VAxhDiWowSBCJIK5MhCj5azyJ8wFwLAKZA6QsZCgZDZD")
+    user.fetch
+    puts user.to_yaml
+  end
+
   def destroy
 
     if Token.destroy_token(params[:id])
