b9c66dccc100666ad52319e52a16a16c518512eb
Mauricio Almeida <mauriciomdea@gmail.com>
2015-10-05 22:52:11 +0000

    Fixing AWS SES config

---
+++ beautboss/config/initializers/amazon_ses.rb
@@ -0,0 +1,4 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  server: "email.us-west-2.amazonaws.com",
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_KEY']
