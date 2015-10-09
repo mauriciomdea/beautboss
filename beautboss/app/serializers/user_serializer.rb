ae477ea651279fd683707d0d3607f947f5d78961
Mauricio Almeida <mauriciomdea@gmail.com>
2015-10-09 04:19:22 +0000

    Método para pegar número de seguidores e seguindo (perfil do usuário, slide 36)

--- beautboss/app/serializers/user_serializer.rb
+++ beautboss/app/serializers/user_serializer.rb
@@ -1,3 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :facebook, :avatar, :website, :location, :bio, :created_at
+  
+  attributes :id, :name, :email, :facebook, :avatar, :website, :location, :bio, :followers, :following, :created_at
+
+  def followers
+    object.followers.size
+  end
+
+  def following
+    object.following.size
+  end
+
 end
