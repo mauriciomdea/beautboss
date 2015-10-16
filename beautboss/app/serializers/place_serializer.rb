757ff7981f23bde6e639653c2b36ca24dcb1e51e
Mauricio Almeida <mauriciomdea@gmail.com>
2015-10-16 08:19:39 +0000

    Added Place ID back on serializer

--- beautboss/app/serializers/place_serializer.rb
+++ beautboss/app/serializers/place_serializer.rb
@@ -1,3 +1,3 @@
 class PlaceSerializer < ActiveModel::Serializer
-  attributes :foursquare_id, :name, :latitude, :longitude, :address, :contact, :website, :created_at
+  attributes :id, :foursquare_id, :name, :latitude, :longitude, :address, :contact, :website, :created_at
 end
