ad6343064607d27d7783e4d60aec4897a1fc89ab
Mauricio Almeida <mauriciomdea@gmail.com>
2015-11-04 04:23:08 +0000

    Ordered Newsfeed by date desc

--- beautboss/app/controllers/api/v1/newsfeed_controller.rb
+++ beautboss/app/controllers/api/v1/newsfeed_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::NewsfeedController < ApplicationController
 
   def show
 
-    newsfeed = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).offset(params[:offset] || 0)
+    newsfeed = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).offset(params[:offset] || 0).order(created_at: :desc)
     count = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).count
     serialized_newsfeed = newsfeed.map { |post| PostSerializer.new(post).as_json(root:false) }
     render json: {count: count, posts: serialized_newsfeed },
--- beautboss/spec/requests/newsfeed_spec.rb
+++ beautboss/spec/requests/newsfeed_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe "Newsfeed API v1", type: :request do
       current_user.follow(john)
       current_user.follow(jane)
       for i in 1..3
-        FactoryGirl.create :post_public, user: john, service: "Post #{i} from #{john.name}", created_at: i.seconds.ago
-        FactoryGirl.create :post_public, user: jane, service: "Post #{i} from #{jane.name}", created_at: i.seconds.ago
-        FactoryGirl.create :post_public, service: "Post #{i} from Someone Else", created_at: i.seconds.ago
+        FactoryGirl.create :post_public, user: john, service: "Post #{i} from #{john.name}", created_at: (3-i).seconds.ago
+        FactoryGirl.create :post_public, user: jane, service: "Post #{i} from #{jane.name}", created_at: (3-i).seconds.ago
+        FactoryGirl.create :post_public, service: "Post #{i} from Someone Else", created_at: (3-i).seconds.ago
       end
       get "/api/v1/newsfeed?limit=5", {}, { "Accept" => "application/json", "HTTP_TOKEN" => valid_auth_token(current_user) }
       expect(response.status).to eq 200 # ok
@@ -27,7 +27,7 @@ RSpec.describe "Newsfeed API v1", type: :request do
       expect(body["count"]).to eq 6
       expect(body["posts"].size).to eq 5
       expect(body["posts"][0]["user"]["id"]).to eq john.id
-      expect(body["posts"][0]["service"]).to eq "Post 1 from John Doe"
+      expect(body["posts"][0]["service"]).to eq "Post 3 from John Doe"
 
     end
 
