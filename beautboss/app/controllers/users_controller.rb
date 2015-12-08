7ec1457b28a17ea595050796ba5b7d7f8b52a680
Mauricio Almeida <mauriciomdea@gmail.com>
2015-12-08 00:54:31 +0000

    Edit profile

--- beautboss/app/controllers/users_controller.rb
+++ beautboss/app/controllers/users_controller.rb
@@ -2,7 +2,25 @@ class UsersController < ApplicationController
   before_action :authenticate_user
 
   def show 
-    @user = @current_user
+    @user = User.find_by!(username: params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @current_user.update(user_params)
+      # render :edit
+      redirect_to edit_user_path(@current_user)
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :avatar, :website, :location, :bio)
+    end
 
 end
