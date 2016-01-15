6b1edb3f1852ff8e16fcd67ec55b33e1c02e7236
Mauricio Almeida <mauriciomdea@gmail.com>
2016-01-15 20:15:32 +0000

    Removing nulls from newsfeed

--- beautboss/app/controllers/api/v1/newsfeed_controller.rb
+++ beautboss/app/controllers/api/v1/newsfeed_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::NewsfeedController < Api::V1::ApiController
     newsfeed = Activity.where("actor_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).offset(params[:offset] || 0).order(created_at: :desc)
     count = Activity.where("actor_id IN (?)", @current_user.following.pluck(:id)).count
     serialized_newsfeed = newsfeed.map { |activity| NewsfeedSerializer.new(activity).as_json(root: false) unless activity.subject_type == "User" }
-    render json: {count: count, news: serialized_newsfeed },
+    render json: {count: count, news: serialized_newsfeed.compact },
       location: "/api/v1/newsfeed/all",
       status: :ok
 
@@ -17,7 +17,7 @@ class Api::V1::NewsfeedController < Api::V1::ApiController
     newsfeed = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).offset(params[:offset] || 0).order(created_at: :desc)
     count = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).count
     serialized_newsfeed = newsfeed.map { |post| RegisterSerializer.new(Register.new(post: post, observer: @current_user)).as_json(root:false) }
-    render json: {count: count, posts: serialized_newsfeed },
+    render json: {count: count, posts: serialized_newsfeed.compact },
       location: "/api/v1/newsfeed/registers",
       status: :ok
 
