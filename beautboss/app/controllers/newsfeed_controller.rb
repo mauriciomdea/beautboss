3f6e520ef562697933c5d943a8bfef6b86226196
Mauricio Almeida <mauriciomdea@gmail.com>
2016-01-21 21:12:20 +0000

    Register page is now visible by anyone

--- beautboss/app/controllers/newsfeed_controller.rb
+++ beautboss/app/controllers/newsfeed_controller.rb
@@ -1,12 +1,13 @@
 class NewsfeedController < ApplicationController
-  # before_action :authenticate_user, except: [:show]
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:show]
+  # before_action :authenticate_user
 
   def index 
     @newsfeed = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).order(created_at: :desc)
   end
 
   def show 
+    current_user
     @post = Post.find(params[:id])
   end
 
