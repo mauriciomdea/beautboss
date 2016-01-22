2e569a312fc483075c184685455a9556ae1be36f
Mauricio Almeida <mauriciomdea@gmail.com>
2016-01-22 03:40:05 +0000

    Fixed posting comments on registers

--- beautboss/app/controllers/comments_controller.rb
+++ beautboss/app/controllers/comments_controller.rb
@@ -2,6 +2,32 @@ class CommentsController < ApplicationController
   before_action :authenticate_user
 
   def create
+    user = @current_user
+    post = Post.find(comment_params[:post_id])
+    @comment = Comment.new(post: post, user: user, comment: comment_params[:comment])
+    if @comment.save
+      # saves activity
+      activity = Activity.create(owner: post.user, actor: user, subject: @comment)
+      # sends notifications
+      unless @current_user == post.user
+        msg = I18n.t('notifications.comment', name: @current_user.name, comment: @comment.comment)
+        device = Device.where(user: post.user).last
+        device.push_notification(msg, ActivitySerializer.new(activity).as_json(root: false)) unless device.nil?
+      end
+      respond_to do |format|
+        format.js
+      end
+    else
+      logger.info @comment.errors.full_messages
+    end
+  rescue ActiveRecord::RecordNotFound
+    logger.info "Post not found"
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:post_id, :comment)
+    end
 
 end
--- beautboss/app/controllers/wows_controller.rb
+++ beautboss/app/controllers/wows_controller.rb
@@ -2,10 +2,15 @@ class WowsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    Wow.create(post: params[:post_id], user: @current_user)
-    respond_to do |format|
-      # format.html { redirect_to wows_url }
-      format.js
+    # logger.debug "oi!"
+    wow = Wow.new(post: params[:post_id], user: @current_user)
+    # logger.debug wow.to_yaml
+    if wow.save
+      # logger.debug wow.errors
+      respond_to do |format|
+        # format.html { redirect_to wows_url }
+        format.js
+      end
     end
   end
 
