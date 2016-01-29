932cece9a7a70c91685caabda3c16cfcdb6f7fee
Mauricio Almeida <mauriciomdea@gmail.com>
2016-01-29 06:39:04 +0000

    Added current user's register to his newsfeed

--- beautboss/app/controllers/newsfeed_controller.rb
+++ beautboss/app/controllers/newsfeed_controller.rb
@@ -3,7 +3,7 @@ class NewsfeedController < ApplicationController
   # before_action :authenticate_user
 
   def index 
-    @newsfeed = Post.where("user_id IN (?)", @current_user.following.pluck(:id)).limit(params[:limit] || 20).order(created_at: :desc)
+    @newsfeed = Post.where("user_id IN (?) OR user_id = ?", @current_user.following.pluck(:id), @current_user.id).limit(params[:limit] || 20).order(created_at: :desc)
   end
 
   def show 
