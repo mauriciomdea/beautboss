cc814341e6540ea2e8f1b388c6c9b976ee6adf96
Mauricio Almeida <mauriciomdea@gmail.com>
2016-01-29 06:22:59 +0000

    Fixed wow button

--- beautboss/app/controllers/wows_controller.rb
+++ beautboss/app/controllers/wows_controller.rb
@@ -2,11 +2,9 @@ class WowsController < ApplicationController
   before_action :authenticate_user
 
   def create
-    # logger.debug "oi!"
-    wow = Wow.new(post: params[:post_id], user: @current_user)
-    # logger.debug wow.to_yaml
-    if wow.save
-      # logger.debug wow.errors
+    @wow = Wow.new(post: Post.find(params[:post_id]), user: @current_user)
+    if @wow.save
+      # puts @wow.errors.to_json
       respond_to do |format|
         # format.html { redirect_to wows_url }
         format.js
@@ -16,6 +14,7 @@ class WowsController < ApplicationController
 
   def destroy
     wow = Wow.find(params[:id])
+    @post = wow.post
     wow.destroy if wow.user == @current_user
     respond_to do |format|
       # format.html { redirect_to wows_url }
