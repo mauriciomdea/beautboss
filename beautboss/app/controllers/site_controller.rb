250ae493b9828fad2d636fb2fdcc161a9ac8a295
Mauricio Almeida <mauriciomdea@gmail.com>
2016-05-17 04:07:33 +0000

    Added pt_BR versions of static pages

--- beautboss/app/controllers/site_controller.rb
+++ beautboss/app/controllers/site_controller.rb
@@ -10,7 +10,20 @@ class SiteController < ApplicationController
   def support
   end
 
+  def terms
+    if I18n.locale == "pt_BR"
+      render "terms.pt_BR"
+    else
+      render "terms"
+    end
+  end
+
   def privacy
+    if I18n.locale == "pt_BR"
+      render "privacy.pt_BR"
+    else
+      render "privacy"
+    end
   end
 
 end
